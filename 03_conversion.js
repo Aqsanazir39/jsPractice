1c993b0b45af73a48802a872291b3bebcbbc86b4
Aqsa Nazir <aqsanazir39@gmail.com>
2024-04-25 15:43:53 +0500

    new updates

--- 03_conversion.js
+++ 03_conversion.js
@@ -18,5 +18,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************************Operations************************* */
+let value = 3;
+let negVal = -value;
+console.log(negVal);
+
+
+let str1 = "hello";
+let str2 = " ali";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1"+ 3);
+// console.log(1 + 2 +(5 * 7) );
+// console.log(1 + 2 + "5");
+// console.log("5" + 2 + 1);
+
+
+console.log(+true);
+console.log(+" "); // not preferred
+let num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
