48287f2c65c1453cc73ea4fef36b44189216fc0f
Aqsa Nazir <aqsanazir39@gmail.com>
2024-04-25 20:11:01 +0500

    new file

--- 05_string.js
+++ 05_string.js
@@ -3,3 +3,8 @@ const names = "aqsa";
 const repoCount = 45;
 
 console.log(`Hello my name is ${names} and my repo count ${repoCount}`); //string enterPolation
+
+
+const gameName = new String ('aqsa');
+
+console.log(gameName[0]);
