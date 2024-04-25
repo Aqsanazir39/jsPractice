d66524952255b0f36d55f4db454d76f9e14de8e1
Aqsa Nazir <aqsanazir39@gmail.com>
2024-04-25 14:08:33 +0500

    name changing

---
+++ 03_conversion.js
@@ -0,0 +1,22 @@
+let score = "abc";
+
+
+// console.log( typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = undefined;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//1 => true // 0 => false
+// "" false
+// "Aqsa" => true
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
