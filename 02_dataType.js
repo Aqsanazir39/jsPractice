a540b2e74f964082a31d2ac15966d95dbbc3178f
Aqsa Nazir <aqsanazir39@gmail.com>
2024-04-25 10:20:59 +0500

    datatypes

---
+++ 02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello"); we are using node js, not browser // tc39 //MDN
+
+console.log(3 +3);
+
+//DataTypes
+
+let name = "Aqsa";
+let age = 23;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null : is a standalone value , it is a type
+//undefined => value not assigned yet
+//symbol => used for uniqueness
+
+
+//object
+
+console.log(typeof "Aqsa");
+
+console.log(typeof null);
+
+console.log(typeof undefined);
--- var.js
+++ var.js
@@ -2,5 +2,10 @@ const accountdId = 9000;
 let accountEmail = "aqsa@gmail.com";
 var accountPassword = "894757";
 accountCity = "Abbottabad";
+let stateee;
 // 
 console.log(accountdId);
+console.table([accountdId, accountEmail, accountPassword, accountCity, stateee]);
+
+/* Prefered not to use var: because of issue in block scope and functional scope
+ */
